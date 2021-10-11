b204f1f81128c71b9f84ab8c68aaa4d9377bc8cc
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2021-10-11 16:58:56 -0300

    Initial commit
    Metada da Aula 02 - Concluida

--- src/routes.js
+++ src/routes.js
@@ -13,9 +13,56 @@ const profile = {
     "vacation-per-year": 4
 }
 
-// req, res
-routes.get('/', (req, res) => res.render(views + 'index'))
+const jobs = [
+    {
+        id: 1,
+        name: "Pizzaria Guloso" ,
+        "daily-hours": 2,
+        "total-hours": 60,
+        created_at: Date.now() // atribuindo data de hoje
+    },
+    {
+        id: 2,
+        name: "OneTwo Project" ,
+        "daily-hours": 3,
+        "total-hours": 47,
+        created_at: Date.now() // atribuindo data de hoje
+    }
+]
+
+routes.get('/', (req, res) => {
+
+
+    const updatedJobs = jobs.map((job) => {
+        // ajustes no Jobs
+        // calculo de tempo restante
+        const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+
+        const createdDate = new Date(job.created_at)
+        const dueDay = createdDate.getDate() + Number(remainingDays)
+        const dueDate = createdDate.setDate()
+
+        return job
+    })
+  
+    
+    return res.render(views + 'index', { jobs })
+
+})
 routes.get('/job', (req, res) => res.render(views + 'job'))
+routes.post('/job', (req, res) => {
+
+    const lastId = jobs[jobs.length - 1]?.id || 1;
+
+    jobs.push({
+        id: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily_hours"],
+        "total-hours": req.body["total_hours"],
+        created_at: Date.now() // atribuindo data de hoje
+    })
+    return res.redirect('/')
+})
 routes.get('/job/edit', (req, res) => res.render(views + 'job-edit'))
 routes.get('/profile', (req, res) => res.render(views + 'profile', { profile }))
 
